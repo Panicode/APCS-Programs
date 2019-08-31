3f883c1b052193c2e48bb1f05816eea27e341af1
Panic <54682874+Panicode@users.noreply.github.com>
2019-08-30 22:07:32 -0700

    Create findingMichael.java

---
+++ unit1b/findingMichael.java
@@ -0,0 +1,18 @@
+public class findingMichael{
+    public static void main(String []args){
+        String artists[]={"Soulja Boy","Obama","Jack Stauber"};
+        for(int i=0;i<artists.length;i++){System.out.println(artists[i]);}
+        System.out.println("");
+        int rand=(int)(Math.random()*3);
+        artists[rand]="Michael Jackson";
+        for(int i=0;i<artists.length;i++){System.out.println(artists[i]);}
+        System.out.println("Michael Jackson has arrived."+"\n");
+        boolean foundMichael=false;
+        for(int i=0;i<artists.length;i++){
+            if(foundMichael==false){System.out.println("No Michael in music("+i+")");foundMichael=false;}
+            if(artists[i].contains("Michael Jackson")){
+                System.out.println("We found Michael, He was in music("+i+")");foundMichael=true;
+            }
+        }
+    }
+}
