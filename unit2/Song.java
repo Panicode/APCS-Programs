430cafeb186e80258dd852a553aa8025e6afe1fe
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-04 09:26:16 -0700

    Create Song.java

---
+++ unit2/Song.java
@@ -0,0 +1,49 @@
+//2.1
+public class Song{
+    String name;
+    int year;
+    String artist;
+    public Song(){
+        name="The good life";
+        year=1996;
+        artist="Weezer";
+    }
+    public Song(String name,int year,String artist){
+        this.name=name;
+        this.year=year;
+        this.artist=artist;
+    }
+    public String toString(){
+        return "The song's name is: "+name+", and the song's date is: "+year+", and the song's artist is: "+artist;
+    }
+    public static void main(String args[]){
+        Song obj=new Song();
+        System.out.println(obj);
+    }
+}
+class SongDriver{
+    String name;
+    int year;
+    String artist;
+    public SongDriver(){
+        name="Megalovania";
+        year=2015;
+        artist="Toby Fox";
+    }
+    public SongDriver(String name,int year,String artist){
+        this.name=name;
+        this.year=year;
+        this.artist=artist;
+    }
+    public String toString(){
+        return "The song's name is: "+name+", and the song's date is: "+year+", and the song's artist is: "+artist;
+    }
+    public static void main(String args[]){
+        Song song1=new Song();
+        Song song2=new Song("Feel Good Inc.",2005,"Gorillaz");
+        System.out.println("Song1:");
+        System.out.println(song1);
+        System.out.println("Song2:");
+        System.out.println(song2);
+    }
+}
