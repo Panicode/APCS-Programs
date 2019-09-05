33528116a23dd5335725831e35ff0256582d731d
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-04 20:36:56 -0700

    Create Clock.java

---
+++ unit2/Clock.java
@@ -0,0 +1,30 @@
+class Clock{
+    int seconds;
+    int minutes;
+    int hours;
+    public Clock(){
+        seconds=0;
+        minutes=0;
+        hours=0;
+    }
+    public Clock(int s,int m,int h){
+        this.seconds=s;
+        this.minutes=m;
+        this.hours=h;
+    }
+    public String toString(){
+       return seconds+":"+minutes+":"+seconds;
+    }
+    public static void main(String args[]){
+        Clock obj=new Clock(1,1,1);
+        System.out.println(obj);
+    }
+}
+class ClockDriver{
+    public static void main(String args[]){
+        Clock kitchenClock=new Clock(2,2,2);
+        System.out.println(kitchenClock);
+        Clock bedroomClock=new Clock(3,3,3);
+        System.out.println(bedroomClock);
+    }
+}
