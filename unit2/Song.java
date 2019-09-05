e41f30699bb8665184d1b00356f28565b7399757
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-04 20:38:51 -0700

    Update Song.java

--- unit2/Song.java
+++ unit2/Song.java
@@ -1,5 +1,5 @@
 //2.1
-public class Song{
+class Song{
     String name;
     int year;
     String artist;
@@ -21,29 +21,10 @@ public class Song{
         System.out.println(obj);
     }
 }
-
-
-
-public class SongDriver{ //This is a new class that was needed for 2.1
-    String name;
-    int year;
-    String artist;
-    public SongDriver(){
-        name="Megalovania";
-        year=2015;
-        artist="Toby Fox";
-    }
-    public SongDriver(String name,int year,String artist){
-        this.name=name;
-        this.year=year;
-        this.artist=artist;
-    }
-    public String toString(){
-        return "The song's name is: "+name+", and the song's date is: "+year+", and the song's artist is: "+artist;
-    }
+public class SongDriver{
     public static void main(String args[]){
-        SongDriver song1=new SongDriver();
-        SongDriver song2=new SongDriver("Feel Good Inc.",2005,"Gorillaz");
+        Song song1=new Song();
+        Song song2=new Song("Feel Good Inc.",2005,"Gorillaz");
         System.out.println("Song1:");
         System.out.println(song1);
         System.out.println("Song2:");
