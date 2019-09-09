fe8367a2eae70c5e735d024b50dafe34a1beffa4
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-09 09:02:04 -0700

    Update Card.java

--- unit2/Card.java
+++ unit2/Card.java
@@ -16,3 +16,14 @@ public class Card{
         cval=b;
     }
 }
+public class CardDriver{
+    public static void main(String []args){
+        Card crd1=new Card("Clubs",5);
+        Card crd2=new Card("hearts",10,"King");
+        Card crd3=new Card("Spades",1);
+        Card crdar[]={crd1,crd2,crd3};
+        for(int i=0;i<crdar.length;i++){
+            System.out.println(crdar[i]);
+        }
+    }
+}
