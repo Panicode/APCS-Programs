4ab1ff1c6ac63816530a645ada61cb5e6cc99440
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-13 09:00:57 -0700

    Update tenSquare.java

--- unit1b/tenSquare.java
+++ unit1b/tenSquare.java
@@ -4,7 +4,8 @@ public class tenSquare{
         for(int i=1;i<=10;i++){
             double ic=Math.pow(i,2);
             long icr=Math.round(ic);
-            int rand=(int)(Math.random()*ic+1);
+            double randch=icr-ic;
+            int rand=(int)(Math.random()*randch+1);
             System.out.println(i+"\t"+icr+"\t"+rand);
         }
 }}
