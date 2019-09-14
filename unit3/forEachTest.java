c41a8e615846c2dca3cb9fc1489be0465ed37914
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-13 21:14:06 -0700

    Create forEachTest.java

---
+++ unit3/forEachTest.java
@@ -0,0 +1,8 @@
+class forEachTest{ // 3.7
+    public static void main(String args[]){
+        String teachers[]={"O'Rourke", "Young", "Bookatub", "Andre", "Brown", "Saul"};
+        for(String i:teachers){
+            System.out.println(i);
+        }
+    }
+}
