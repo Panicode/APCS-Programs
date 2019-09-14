9705a54958de9b3a33bea33f8f03f877dad62696
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-13 20:57:19 -0700

    Update intSearch.java

--- unit3/intSearch.java
+++ unit3/intSearch.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 public class intSearch{
-    int inte[]={-9,2,3,4,7,9,10,23,45,67,41,22,35,21,92,2048,44,51,32,89};
+    int inte[]={-9,2,3,4,7,9,10,23,45,67,41,22,35,21,92,2048,44,51,32,89}; //possible valid inputs
     int finder(int find){
         boolean check=false;
         for(int i=0;i<inte.length;i++){
