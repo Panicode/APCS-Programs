1a0ceaaf7c1eea96b8a807682029a5ae35161331
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-16 09:08:24 -0700

    Update WarmUp.java

--- etcProjects/WarmUp.java
+++ etcProjects/WarmUp.java
@@ -4,12 +4,17 @@ public class WarmUp{
     public static void main(String []args){
         int arr[]=new int[20];
         for(int i=0;i<arr.length;i++){
-            arr[i]=i+50;
+            arr[i]=(int)(Math.random()*20)+51;
         }
+        boolean iffound=false;
         for(int i=0;i<arr.length;i++){
             if(arr[i]==55){
                 System.out.println("The number 55 was found at the index of: "+i);
+                iffound=true;
             }
+        }
+        if(iffound==false){
+        System.out.println("The number 55 was not found");
         }
     }
 }
