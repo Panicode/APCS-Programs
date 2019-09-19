a164b9751caecf031ce2e0fb99e152344ba665db
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-19 11:33:56 -0700

    Rename MultiTable to MultiTable.java

---
+++ unit5/MultiTable.java
@@ -0,0 +1,16 @@
+public class MultiTable{
+    public static void main(String args[]){
+        int arr[][]=new int[8][8];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=(i+1)*(j+1);
+            }
+        }
+        for(int k=0;k<arr.length;k++){
+            for(int l=0;l<arr[0].length;l++){
+                System.out.print(arr[k][l]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
