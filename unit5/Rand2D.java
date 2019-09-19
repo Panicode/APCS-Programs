872db49fa546333266573ac5633c9fa07d346a56
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-19 10:51:22 -0700

    Update Rand2D.java

--- unit5/Rand2D.java
+++ unit5/Rand2D.java
@@ -1,14 +1,14 @@
 public class Rand2D{
-    public static void main(String[] args){
-        int [][] arr = new int [3][4];
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 4; j++){
-                arr[i][j] =  (int) (Math.random()* 9) ;
+    public static void main(String args[]){
+        int arr[][]=new int[3][4];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=(int)(Math.random()*9);
             }
         }
-        for(int k = 0; k < 3; k++){
-            for(int l = 0; l < 4; l++){
-                System.out.print(arr[k][l] + " ");
+        for(int k=0;k<arr.length;k++){
+            for(int l=0;l<arr[0].length;l++){
+                System.out.print(arr[k][l]+" ");
             }
             System.out.println();
         }
