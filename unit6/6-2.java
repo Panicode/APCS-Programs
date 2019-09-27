74d8159775b2d4fff3a5ffe834a81bd9e364a612
Panic <54682874+Panicode@users.noreply.github.com>
2019-09-27 08:25:22 -0700

    Update 6-2.java

--- unit6/6-2.java
+++ unit6/6-2.java
@@ -1,7 +1,16 @@
-public class Games{
-    
+import java.util.ArrayList;
+class Games{
+    public void PlayGame(){
+        System.out.println("Let's play a game!");
+    }
+    public void PlaySmash(){
+        System.out.println("Hello! Wanna play smash?");
+    }
 }
 class CounterStrike extends Games{
+    public void PlayGame(){
+        System.out.println("Let's play Counter Strike!");
+    }
     String map;
     CounterStrike(){
         map="Dust 2";
@@ -14,6 +23,9 @@ class CounterStrike extends Games{
     }
 }
 class RainbowSix extends Games{
+    public void PlayGame(){
+        System.out.println("Let's play Rainbow Six Siege!");
+    }
     int toxicity=100;
     public void CheckToxicity(){
         if(toxicity>50){
@@ -22,7 +34,28 @@ class RainbowSix extends Games{
     }
 }
 class SmashBros extends Games{
-    public void Bruh(){
-        System.out.println("bruh");
+    public void PlayGame(){
+        System.out.println("Let's play Smash Bros!");
+    }
+    public void PlaySmash(){
+        System.out.println("wanna play smash??? ok final destination 3 stock no items idiot haha");
+    }
+}
+public class GamesDriver{
+    public static void main(String args[]){
+        Games games[]=new Games[3];
+        games[0]=new CounterStrike();
+        games[1]=new RainbowSix();
+        games[2]=new SmashBros();
+        ArrayList<Games> gameList=new ArrayList<Games>();
+        gameList.add(new CounterStrike());
+        gameList.add(new RainbowSix());
+        gameList.add(new SmashBros());
+        for(int i=0;i<games.length;i++){
+            games[i].PlayGame();
+        }
+        for(int i=0;i<gameList.size();i++){
+            gameList.get(i).PlayGame();
+        }
     }
 }
