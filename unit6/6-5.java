6ec94563aeeb989d52afaba442e1686aa93719f2
Panic <54682874+Panicode@users.noreply.github.com>
2019-10-07 14:31:35 -0700

    Update 6-5.java

--- unit6/6-5.java
+++ unit6/6-5.java
@@ -1,1 +1,34 @@
+class MyPolygon{
+    String name="";
+    String color="";
+    int leg1=0;
+    int leg2=0;
+    MyPolygon(String na, String co, int le1, int le2){
+        name=na;
+        color=co;
+        leg1=le1;
+        leg2=le2;
+    }
+    String ToString(){
+        return name+"'s variables:"+"\ncolor: "+color+"\nleg1: "+leg1+"\nleg2: "+leg2;
+    }
+    String getFirstLeg(){
+        return leg1;
+    }
+    String getSecondLeg(){
+        return leg2;
+    }
+}
+class MyRect extends MyPolygon{
+    
+}
 
+
+
+
+
+public class Main(){
+    public static void main(String[] args){
+        
+    }
+}
