3eee7403e463412d0d6447ead92472d5d0a97b29
Panic <54682874+Panicode@users.noreply.github.com>
2019-10-08 13:21:34 -0700

    Update Favorites.java

--- unit1a/Favorites.java
+++ unit1a/Favorites.java
@@ -1,8 +1,4 @@
-/**Write a program titled MyFavorites.java
-that prints your “favorites”. The program should print your favorite food, favorite color
-favorite movie, favorite musical group and favorite class at San Clemente High. Each favorite should be on its own line. **/
 public class Favorites{
-
      public static void main(String []args){
         System.out.println("My favorites are:");
         System.out.println("My favorite food is Italian Ice.");
