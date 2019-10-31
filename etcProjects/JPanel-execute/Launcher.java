10768d9f50c426fd5787bae5f16bb44f65772532
Panic <54682874+Panicode@users.noreply.github.com>
2019-10-31 12:00:12 -0700

    Create Launcher.java

---
+++ etcProjects/JPanel-execute/Launcher.java
@@ -0,0 +1,10 @@
+package com.panic.main;
+public class Launcher {
+    public Launcher(){
+        new Window();
+    }
+    public static void main(String[] args){
+        new Launcher();
+        System.out.println("Execution successful.");
+    }
+}
