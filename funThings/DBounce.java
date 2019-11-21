79dce7698fe99490a24cfb5eb8b8d2d25210fe01
Panic <54682874+Panicode@users.noreply.github.com>
2019-11-21 08:00:50 -0800

    Update DBounce.java

--- funThings/DBounce.java
+++ funThings/DBounce.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.net.URL;
 public class Launcher{
     public static void main(String[] args){
-        Game game= new Game("TBG",640,360);
+        Game game= new Game("DVD Screensaver",640,360);
         game.start();
     }
 }
