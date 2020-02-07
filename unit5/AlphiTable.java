f18cad793c15062761e247898fb17b263fb431ac
Panic <54682874+Panicode@users.noreply.github.com>
2020-02-07 09:20:26 -0800

    tabbing update
    it sucks

--- unit5/AlphiTable.java
+++ unit5/AlphiTable.java
@@ -1,21 +1,24 @@
 //5.4
 public class AlphiTable{
     public static void main(String args[]){
-        String arr[][]=new String[8][8];
+        String arr[][]=new String[16][3];
         char che='a';
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                arr[i][j]=""; //defining variable so it isn't null
-                arr[i][j]+=che;
+        boolean check=true;
+        for(int k=0;k<arr.length;k++){
+            for(int l=0;l<arr[0].length;l++){
+                arr[k][l]=""; //defining variable so it isn't null
+                arr[k][l]+=che;
+                if(check){
+                    System.out.print(arr[k][l]+" ");
+                }else{
+                    System.out.print(" "+arr[k][l]);
+                    if(l>=arr[0].length-2)l++;
+                }
                 if(che=='z'){
                     che='a';
                 }else{che++;}
             }
-        }
-        for(int k=0;k<arr.length;k++){
-            for(int l=0;l<arr[0].length;l++){
-                System.out.print(arr[k][l]+" ");
-            }
+            check = !check;
             System.out.println();
         }
     }
